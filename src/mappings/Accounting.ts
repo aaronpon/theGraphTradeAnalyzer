ffbe502b45c91e6069f108310a4d301b8a39c38a
aaronpon <aaronpon@Live.com>
2020-12-21 00:32:12 +0800

    Initial commit

---
+++ src/mappings/Accounting.ts
@@ -0,0 +1,9 @@
+import { AssetAddition, AssetRemoval } from '../generated/AccountingContract';
+
+export function handleAssetAddition(event: AssetAddition): void {
+  // createFundEvent('AssetAddition', context);
+}
+
+export function handleAssetRemoval(event: AssetRemoval): void {
+  // createFundEvent('AssetRemoval', context);
+}
---
+++ src/mappings/UserWhitelistPolicy.ts
@@ -0,0 +1,9 @@
+import { ListRemoval, ListAddition } from '../generated/UserWhitelistPolicyContract';
+
+export function handleListAddition(event: ListAddition): void {
+  // TODO
+}
+
+export function handleListRemoval(event: ListRemoval): void {
+  // TODO
+}
